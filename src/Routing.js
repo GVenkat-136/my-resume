28f450a98ac04627308e615c815219b2bd4e6000
GVENKATANARENDRA <103510399+GVenkat-136@users.noreply.github.com>
2024-03-11 02:47:37 +0530

    Add files via upload

---
+++ src/Routing.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import { BrowserRouter as MainRouter, Route, Routes } from 'react-router-dom'
+import Main from './components/Main'
+import Home from './components/Home'
+import Projects from './components/Projects'
+import Contact from './components/Contact'
+import SocialMedia from './components/SocialMedia'
+import Skills from './components/Skilles'
+import Resume from './components/Resume'
+
+
+export default function Routing() {
+   return (
+      <MainRouter>
+         <Routes >
+            <Route path='/' element={<Main />} >
+               <Route index element={<Home />} />
+               <Route path='Skills' element={<Skills />} />
+               <Route path='Projects' element={<Projects />} />
+               <Route path='SocialMedia' element={<SocialMedia />} />
+               <Route path='contact' element={<Contact />} />
+               <Route path='Resume' element={<Resume />} />
+            </Route>
+         </Routes>
+      </MainRouter>
+   )
+}
---
+++ src/components/Main.js
@@ -0,0 +1,35 @@
+import React from 'react'
+import { Link, Outlet, useLocation } from 'react-router-dom'
+import './Main.css'
+
+export default function Main() {
+   const location = useLocation();
+   const { pathname } = location;
+   return (
+      <div className='container'>
+         <div className='wrapper'>
+            <div className='header'>
+               <div className='Header_wrapper'>
+                  <div className='Logo'>
+                     Venkata <span>Narendra</span>
+                  </div>
+                  <div className='list'>
+                     <Link to='/' className={pathname === '/' && 'isActive'}>Home</Link>
+                     <Link to='Skills' className={pathname === '/Skills' && 'isActive'}>Skills </Link>
+                     <Link to='Projects' className={pathname === '/Projects' && 'isActive'}>Projects</Link>
+                     <Link to='SocialMedia' className={pathname === '/SocialMedia' && 'isActive'}>SocialMedia</Link>
+                     <Link to='contact' className={pathname === '/contact' && 'isActive'}>contact</Link>
+                     <Link to='Resume' className={pathname === '/Resume' && 'isActive'} >Resume</Link>
+                  </div>
+               </div>
+            </div>
+            <div className='content'>
+               <Outlet />
+            </div>
+            <div className='Footer'>
+               Footer
+            </div>
+         </div>
+      </div>
+   )
+}
