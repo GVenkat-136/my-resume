5aa249e1cd924dd1e92d75bb5bdb4910d4db0924
Gnarendra <dev.narendra@icloud.com>
2024-03-16 03:20:00 +0530

    Skills rendering completed

--- src/components/Skilles.js
+++ src/components/Skilles.js
@@ -1,29 +1,62 @@
-import React from 'react'
-import { SkillesList } from './utilities/SkilesData'
+import React, { useState } from 'react'
+import { SkillesList, SkillesListData } from './utilities/SkilesData'
 export default function Skilles() {
+   const [openSkill, setOpenSkill] = useState('CSS')
    return (
       <div class="Skills_container">
          <div class='skills_wrapper'>
-            {
-               SkillesList && SkillesList?.map((technology, index) => {
-                  return (
-                     <div className="card" key={index}>
+            <div className='Skills_nav'>
+               <div className='nav_wrapper'>
+                  {
+                     SkillesListData && SkillesListData?.map((icon, i) => {
+                        return (
+                           <li key={i} style={{ background: openSkill === icon?.label ? icon?.color : '', color: openSkill === icon?.label ? "#fff" : '' }} onClick={() => setOpenSkill(icon?.label)}>
+                              <span>
+                                 {icon.label}
+                              </span>
+                           </li>
+                        )
+                     })
+                  }
+               </div>
+            </div>
+            <div className='Skille_content'>
+               <div className='Content_title'>
+                  <span style={{ color: SkillesList[openSkill]?.color }}>{SkillesList[openSkill]?.label} :</span>
+               </div>
+               <div className='Skill_Conent_wrapper'>
+                  <div className='Card_wrapper'>
+                     <div className="card" >
                         <div className="percent">
-                           <svg >
-                              <circle cx="75" cy="75" r="70"></circle>
-                              <circle cx="75" cy="75" r="70" style={{ '--percent': `${technology?.percent}`, stroke: technology?.color, }}></circle>
+                           <svg>
+                              <circle cx="125" cy="125" r="120"></circle>
+                              <circle cx="125" cy="125" r="120" style={{ '--percent': `${SkillesList[openSkill]?.percent}`, stroke: SkillesList[openSkill]?.color }}></circle>
                            </svg>
                            <div className="number">
-                              <img src={technology?.src} alt={technology?.label} />
+                              <img src={SkillesList[openSkill]?.src} alt={SkillesList[openSkill]?.label} />
                            </div>
                         </div>
                         <div className="title">
-                           <h2>{technology?.label}</h2>
+                           {/* <h2>{SkillesList[openSkill]?.label}</h2> */}
                         </div>
                      </div>
-                  )
-               })
-            }
+                  </div>
+                  <div className='Card_content'>
+                     <div className='Contnet_List'>
+                        {
+                           SkillesList[openSkill]?.List?.map((s, i) => {
+                              return (
+                                 <li key={i}>
+                                    <b>{s?.split(':')[0]} : </b>
+                                    {s?.split(':')[1]}
+                                 </li>
+                              )
+                           })
+                        }
+                     </div>
+                  </div>
+               </div>
+            </div>
          </div>
       </div >
    )
--- src/components/utilities/SkilesData.js
+++ src/components/utilities/SkilesData.js
@@ -1,63 +1,207 @@
-export const SkillesList = [
-   {
+export const SkillesList = {
+   'CSS': {
       src: '/assets/css/icons8-css-480.svg',
       label: 'CSS',
       percent: 100,
-      color: "#3498db"
+      color: "#3498db",
+      List: [
+         `Basic Styling: Can style HTML elements with colors, fonts, and backgrounds.`,
+         `Layout Control: Understands how to control spacing and arrangement of elements.`,
+         `Flexbox Usage: Able to use Flexbox for layout tasks.`,
+         `Basic Animation Skills: Capable of adding simple animations and transitions.`,
+         `Debugging: Can troubleshoot and fix CSS issues using browser developer tools.`,
+         `Preprocessors Familiarity: Aware of tools like SASS or LESS for organized stylesheets.`,
+         `Responsive Design: Understanding of making designs responsive using CSS techniques.`,
+         `Accessibility Consideration: Aware of techniques to improve accessibility through CSS.`
+      ]
    },
-   {
+   'JavaScript': {
       src: '/assets/js/icons8-js-480.svg',
       label: 'JavaScript',
       percent: 100,
-      color: "#ffd500"
+      color: "#ffd500",
+      List: [
+         'Syntax: Understands basic JavaScript syntax, including variables and functions.',
+         'DOM Manipulation: Can change webpage content using JavaScript.',
+         'Event Handling: Knows how to respond to user actions like clicks.',
+         'Control Flow: Understands if statements and loops to control program flow.',
+         'Functions and Scope: Able to define functions and manage variable scope.',
+         'Arrays and Objects: Familiar with working with arrays and objects.',
+         'Error Handling: Can handle errors to prevent crashes.',
+         'Debugging: Capable of troubleshooting JavaScript code using browser tools.'
+      ]
    },
-   {
+   'ReactJs': {
       src: '/assets/Reactjs/icons8-react-js-400.svg',
       label: 'ReactJs',
       percent: 100,
-      color: "#39c2d7"
+      color: "#39c2d7",
+      List: [
+         'Component Basics: Understands how to create and use React components.',
+         'State Management: Can manage state within React components.',
+         'Props Handling: Knows how to pass data between components using props.',
+         `Lifecycle Methods: Familiar with React's lifecycle methods and their usage.`,
+         'Event Handling: Can handle events within React components.',
+         'Conditional Rendering: Able to conditionally render components based on state or props.',
+         'Redux Integration: Understands how to integrate Redux for state management in React applications.',
+         'React Hooks: Familiar with React Hooks for managing state.'
+      ]
    },
-   {
+   'NodeJs': {
       src: '/assets/nodejs/icons8-nodejs-480.svg',
       label: 'NodeJs',
       percent: 100,
-      color: "#4caf4f"
+      color: "#4caf4f",
+      List: [
+         `Basic Node.js Concepts: Understanding of Node.js fundamentals like event-driven architecture and asynchronous programming.`,
+         `Creating Servers: Can create HTTP servers using Node.js's built-in HTTP module.`,
+         `Express.js Framework: Familiarity with Express.js for building web applications and APIs.`,
+         `Routing: Ability to define routes and handle HTTP requests in Express.js.`,
+         `Middleware: Understanding of middleware and its usage in Express.js for request processing.`,
+         `Working with NPM: Familiar with npm for package management and dependency handling.`,
+         `Database Interaction: Capable of interacting with databases like MongoDB or MySQL using Node.js.`,
+         `Error Handling: Knows how to handle errors and exceptions in Node.js applications.`
+      ]
    },
-   {
+   'Mongodb': {
       src: '/assets/mongodb/icons8-mongodb-480.svg',
       label: 'Mongodb',
       percent: 100,
+      color: "#4caf50",
+      List: [
+         `Database Concepts: Understands basic database terms like collections, documents, and queries.`,
+         `CRUD Operations: Can create, read, update, and delete data in MongoDB.`,
+         `Data Design: Able to design effective database structures.`,
+         `Indexing: Knows how to improve query performance using indexing.`,
+         `Aggregation: Understands how to analyze and manipulate data using aggregation framework.`,
+         `Data Validation: Can enforce rules to ensure data quality.`,
+         `Scaling Concepts: Aware of concepts like replication and sharding for scaling MongoDB databases.`,
+         `Backup and Restore : Familiar with backing up and restoring MongoDB data.`,
+      ]
+
+   },
+   'Redis': {
+      src: '/assets/redis/icons8-redis-480.svg',
+      label: 'Redis',
+      percent: 100,
+      color: "#b71c1c",
+      List: [
+         `Key-Value Store: Understanding of Redis as a key-value store database.`,
+         `Data Structures: Familiarity with Redis data structures like strings, lists, sets, and hashes.`,
+         `Basic Commands: Knowledge of basic Redis commands for data manipulation and retrieval.`,
+         `Pub/Sub Messaging: Understanding of Redis Pub/Sub messaging for real-time communication.`,
+         `Cache Implementation: Ability to use Redis as a caching solution for improving application performance.`,
+         `Basic Configuration: Familiar with basic Redis configuration settings and options.`,
+         `Scaling: Awareness of scaling strategies and techniques for Redis in larger `,
+      ]
+   },
+   'AWS-Lambda': {
+      src: '/assets/lambda/icons8-awslambda-480.svg',
+      label: 'AWS-Lambda',
+      percent: 100,
+      color: "#f78e05",
+      List: [
+         `Serverless Basics: Understands AWS Lambda's role in serverless computing.`,
+         `Function Creation: Can make Lambda functions using AWS Console or SDK.`,
+         `Event Handling: Familiar with triggering functions from sources like API Gateway or S3.`,
+         `Programming Languages: Proficient in languages supported by Lambda.`,
+         `Configuration: Knows how to set up Lambda function settings.`,
+         `Deployment: Can put Lambda functions on AWS and manage them.`,
+         `Monitoring: Understands how to watch Lambda functions with AWS CloudWatch.`,
+         `Security: Aware of keeping Lambda functions secure with best practices.`,
+      ]
+   },
+   'AWS-S3': {
+      src: '/assets/s3/icons8-amazon-s3-480.svg',
+      label: 'AWS-S3',
+      percent: 100,
+      color: "#f78e05",
+      List: [
+         `Object Storage: Understands S3 as a service for storing and retrieving data.`,
+         `Bucket Creation: Can create S3 buckets and adjust their settings.`,
+         `Upload and Download: Familiar with adding and retrieving files from S3 buckets.`,
+         `Storage Classes: Knows about different storage options like Standard and Glacier.`,
+         `Versioning: Aware of maintaining multiple versions of files in S3 buckets.`,
+         `Access Control: Understands how to set permissions for accessing S3 buckets.`,
+         `Lifecycle Policies: Familiar with automating object management in S3 buckets.`,
+         `Replication : Aware of replicating objects across AWS regions for backup and recovery.`,
+      ]
+   },
+   'Socket.io': {
+      src: '/assets/socktet/socketio-icon.svg',
+      label: 'Socket.io',
+      percent: 100,
+      color: "#010101",
+      List: [
+         `Real-Time Communication: Understands Socket.IO for real-time communication between clients & servers.`,
+         `Setup: Can set up Socket.IO for client-server connection.`,
+         `Events Handling: Knows how to send and receive events between client and server.`,
+         `Broadcasting: Capable of sending messages to multiple clients using Socket.IO.`,
+         `Error Handling: Understands basic error handling in Socket.IO.`,
+         `Rooms: Awareness of grouping clients for specific communication.`,
+         `Acknowledgements: Knows how to confirm message receipt between client and server.`,
+         `Integration: Familiar with integrating Socket.IO with other frameworks.`,
+      ]
+   },
+   'Tools': {
+      src: '/assets/Tools/icons8-tools-100.png',
+      label: 'Tools',
+      percent: 100,
+      color: "#010101",
+      List: [
+         `Code Editor: Uses a code editor for writing and editing code.`,
+         `Version Control (Git): Understands Git for managing code changes and collaboration.`,
+         `API Testing: Familiar with tools for testing APIs during development.`,
+         `Browser Tools: Utilizes browser-based tools for debugging and profiling.`,
+         `Package Management: Aware of package managers for handling dependencies.`,
+         `Bundling and Transpiling: Understands tools for optimizing JavaScript code.`,
+         `Debugging Tools: Aware of tools for debugging JavaScript applications.`,
+         `Code Quality Tools: Utilizes tools for maintaining code quality and consistency.`
+      ]
+   },
+}
+
+
+export const SkillesListData = [
+   {
+      label: 'CSS',
+      color: "#3498db"
+   },
+   {
+      label: 'JavaScript',
+      color: "#ffd500"
+   },
+   {
+      label: 'ReactJs',
+      color: "#39c2d7"
+   },
+   {
+      label: 'NodeJs',
+      color: "#4caf4f"
+   },
+   {
+      label: 'Mongodb',
       color: "#4caf50"
 
    },
    {
-      src: '/assets/redis/icons8-redis-480.svg',
       label: 'Redis',
-      percent: 100,
       color: "#b71c1c"
    },
    {
-      src: '/assets/lambda/icons8-awslambda-480.svg',
       label: 'AWS-Lambda',
-      percent: 100,
       color: "#f78e05"
    },
    {
-      src: '/assets/s3/icons8-amazon-s3-480.svg',
       label: 'AWS-S3',
-      percent: 100,
       color: "#f78e05"
    },
    {
-      src: '/assets/socktet/socketio-icon.svg',
       label: 'Socket.io',
-      percent: 100,
       color: "#010101"
    },
    {
-      src: '/assets/git/icons8-git-480.svg',
-      label: 'Git',
-      percent: 100,
-      color: "#f4501e"
+      label: 'Tools',
+      color: "#010101",
    },
 ]
