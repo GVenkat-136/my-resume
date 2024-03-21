ec00f2389aa9e7a95848024073b14bd14841b0b4
Gnarendra <dev.narendra@icloud.com>
2024-03-21 08:45:52 +0530

    layout changes

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,6 +1,9 @@
 import React from 'react'
 import { Link, Outlet, useLocation } from 'react-router-dom'
 import './Main.css'
+import { SlUser, SlBriefcase } from "react-icons/sl";
+import { FaRegFileAlt } from "react-icons/fa";
+
 
 export default function Main() {
    const location = useLocation();
@@ -14,12 +17,6 @@ export default function Main() {
                      Venkata <span>Narendra</span>
                   </div>
                   <div className='list'>
-                     <Link to='/' className={pathname === '/' && 'isActive'}>Home</Link>
-                     <Link to='Skills' className={pathname === '/Skills' && 'isActive'}>Skills </Link>
-                     <Link to='Projects' className={pathname === '/Projects' && 'isActive'}>Projects</Link>
-                     <Link to='SocialMedia' className={pathname === '/SocialMedia' && 'isActive'}>SocialMedia</Link>
-                     <Link to='contact' className={pathname === '/contact' && 'isActive'}>contact</Link>
-                     <Link to='Resume' className={pathname === '/Resume' && 'isActive'} >Resume</Link>
                   </div>
                </div>
             </div>
@@ -27,10 +24,27 @@ export default function Main() {
                <Outlet />
             </div>
             <div className='Footer'>
-               <div className='Footer_wrapper'>
-                  <li>G.venkataNarendra</li>
-                  <li>Hyderabad</li>
-                  <li>dev.gvnarendra@gmail.com</li>
+               <div className='list'>
+                  <Link to='/' className={pathname === '/' && 'isActive'}>
+                     <SlUser />
+                     About
+                  </Link>
+                  <Link to='Skills' className={pathname === '/Skills' && 'isActive'}>
+
+                     Skills
+                  </Link>
+                  <Link to='Projects' className={pathname === '/Projects' && 'isActive'}>
+                     <SlBriefcase />
+                     Work
+                  </Link>
+                  <Link to='contact' className={pathname === '/contact' && 'isActive'}>contact</Link>
+                  <Link to='Resume' className={pathname === '/Resume' && 'isActive'} >
+                     <FaRegFileAlt />
+                     Resume
+                  </Link>
+                  <Link to='Resume' className={pathname === '/Resume' && 'isActive'} >Blog</Link>
+                  <Link to='Resume' className={pathname === '/Resume' && 'isActive'} >Resume</Link>
+                  <Link to='Resume' className={pathname === '/Resume' && 'isActive'} >Resume</Link>
                </div>
             </div>
          </div>
